0d1cde028f9d53cdbc508de28d0f32aa60f78217
jaeh <jascha@jaeh.at>
2021-09-24 00:06:34 +0200

    use EarlyBird from theme

--- src/pages/index.mjs
+++ src/pages/index.mjs
@@ -6,33 +6,7 @@ export const View = state => [
   Hero(state),
 
   div({ id: 'about' }, [
-    h2('early bird <3'),
-
-    p(`
-hello, you are here a bit early.
-feel free to poke around,
-but as long as this notice is here,
-most documentation on this page will not be accurate (yet).
-`),
-
-    p(`
-Since we do all development of this platform in the open, you are looking at a partially working minimum viable product here.
-`),
-
-    h3('What is working'),
-    p([
-      'Some services (',
-      Link({ to: 'https://github.com/grundstein/gps', text: 'gps' }),
-      ', ',
-      Link({ to: 'https://github.com/grundstein/grs', text: 'grs' }),
-      ', ',
-      Link({ to: 'https://github.com/grundstein/gss', text: 'gss' }),
-      ') work fine, and despite their warning labels, are in production on the ',
-      Link({ to: 'https://artificialmuseum.com', text: 'Artificial Museum' }),
-      ' platform since November of 2020, unfortunately the ',
-      Link({ to: 'https://github.com/grundstein/cli', text: 'cli' }),
-      ' and all other parts of the system setup are still work in progress.',
-    ]),
+    EarlyBird(),
 
     h1('grundstein?'),
 
